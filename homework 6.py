8f1d0c099fd6172741c1f9a4f2b619eb3bf44ac4
Mowaffak01 <mowaffakbarakat01@gmail.com>
2026-04-17 09:39:24 +0300

    Add files via upload

---
+++ homework 6.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.Screen().bgcolor("blue")
+sc = turtle.Screen()
+sc.setup(300,300)
+
+for i in range (1,4+1):
+    turtle.forward(100)
+    turtle.right(90)
+turtle.done()
+    
